8a7a6e030cfbf08bc6e930c5949c4d5d517ab989
Isadora da Silva Bispo <158217976+IsaSilvB007@users.noreply.github.com>
2024-06-14 14:40:41 -0300

    Adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(600, 600);
+    background("gray");
+  }
+  
+  function draw() {
+    stroke("white");
+    fill("black");
+    
+    if(mouseIsPressed){
+      rect(mouseX, mouseY, 20, 35);
+    }
+    
+  }
